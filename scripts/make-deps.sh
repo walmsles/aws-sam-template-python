80d81e9c4087551b5f128fc70afe0709e8017cc7
walmsles <2704782+walmsles@users.noreply.github.com>
2023-07-22 13:42:07 +1000

    fix(make-deps): Enable export of central dependencies and handle when group deps don't exist for a service (no poetry errors)

--- scripts/make-deps.sh
+++ scripts/make-deps.sh
@@ -4,11 +4,25 @@ for folder in $(ls -d ${WORKDIR:-${PWD}}/services/* | grep -v __pycache__)
 do
     if [[ -d ${folder} ]]; then
         group=$(basename ${folder})
-        if [[ $(poetry show --with=${group} --without=dev) ]]; then
+
+        # check if service group exists (to avoid poetry errors)
+        poetry show --with=${group} --quiet
+        group_exists=$?
+        all_groups=""
+        if [[ $group_exists -eq 0 ]]; then
+            # export main dependencies AND specific group dependencies
             echo creating requirements in ${folder}
-            poetry export --without-hashes --with=${group} > ${folder}/requirements.txt
+
+            # collect groups to export for testing which requires ALL deps
+            all_groups="${all_groups} --with=${group}"
+            poetry export --without-hashes --with=${group} --with main > ${folder}/requirements.txt
+        else 
+            # export main only - so get centrally installed dependencies
+            echo creating requirements in ${folder}
+            poetry export --without-hashes --with main > ${folder}/requirements.txt
         fi
     fi
 done
 
-poetry export --without-hashes --with dev > ${WORKDIR:-${PWD}}/tests/requirements.txt
+# export for testing
+poetry export --without-hashes --with dev $all_groups > ${WORKDIR:-${PWD}}/tests/requirements.txt
