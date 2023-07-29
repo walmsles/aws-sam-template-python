8e83e9a7f3e107b69d2cc9d03579ad2fe1708b22
walmsles <2704782+walmsles@users.noreply.github.com>
2023-07-30 00:03:40 +1000

    fix(rev ruff): not all linting supported as yet by ruff

--- services/hello_world/app.py
+++ services/hello_world/app.py
@@ -1,15 +1,14 @@
+from aws_lambda_powertools import Logger, Metrics, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
-from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.logging import correlation_paths
-from aws_lambda_powertools import Logger
-from aws_lambda_powertools import Tracer
-from aws_lambda_powertools import Metrics
 from aws_lambda_powertools.metrics import MetricUnit
+from aws_lambda_powertools.utilities.typing import LambdaContext
 
 app = APIGatewayRestResolver()
 tracer = Tracer()
 logger = Logger()
 metrics = Metrics(namespace="Powertools")
+
 
 @app.get("/hello")
 @tracer.capture_method
@@ -22,6 +21,7 @@ def hello():
     # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/logger/
     logger.info("Hello world API - HTTP 200")
     return {"message": "hello world"}
+
 
 # Enrich logging with contextual information from Lambda
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
